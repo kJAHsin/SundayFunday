21a50fd892ea796acd3fc354781c747c1d33133a
kJAHsin <kjahnsencollins@gmail.com>
2022-12-04 21:36:04 +0100

    added quiz-app

---
+++ quiz-app/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    },
+    {
+        question: 'What is the most used programming language in 2019?',
+        a: 'java',
+        b: 'css',
+        c: 'javascript',
+        d: 'python',
+        correct: 'a'
+    },
+    {
+        question: 'Who is the President of the US?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a: 'hypertext markup language',
+        b: 'cascading style sheet',
+        c: 'jason',
+        d: 'python',
+        correct: 'a'
+    },
+    {
+        question: 'What year was js launched?',
+        a: '1996',
+        b: '2019',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'd'
+    }
+]
